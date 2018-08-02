f91b39ac56decf54edc174fd8dd614fe7bea8b63
abc <abc@example.com>
2018-08-02 10:31:19 +0700

    message here

--- src/app/commands.service.ts
+++ src/app/commands.service.ts
@@ -11,15 +11,9 @@ export class CommandsService {
 
   commands = [
     {
-<<<<<<< HEAD
       name:"one", 
       key:[1]
     }, 
-=======
-      name: 'Align Text Left',
-      key: [17, 16, 76]
-    },
->>>>>>> 33993e667e18a25a4a3cd22f3b8026eaac00ff59
     {
       name: 'two',
       key: [50]
--- src/app/gameboard/gameboard.component.ts
+++ src/app/gameboard/gameboard.component.ts
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-import { Component, OnInit,HostListener } from '@angular/core';
-import { CommandsService } from "../commands.service";
-import { MicrosoftExcelService } from "../microsoft-excel.service";
-import { SharedServiceService } from "../shared-service.service";
-//https://www.youtube.com/watch?v=I317BhehZKM&t=105s
-=======
 import { Component, OnInit, HostListener } from '@angular/core';
 import { CommandsService } from '../commands.service';
 import { MicrosoftExcelService } from '../microsoft-excel.service';
-import { ChangeDetectorRef } from '@angular/core';
 
->>>>>>> 33993e667e18a25a4a3cd22f3b8026eaac00ff59
 
 export enum KEY_CODE {
   RIGHT_ARROW = 39,
@@ -23,50 +14,7 @@ export enum KEY_CODE {
   styleUrls: ['./gameboard.component.css']
 })
 export class GameboardComponent {
-<<<<<<< HEAD
- message:any={};
-
-
- value:number= 0;
- name:string;
- names:any=[]=[];
- showName:any;
- key:number;
- keys:any=[]=[];
- showKey:any;
- command:any;
- shortcuts:any=[]=[];
- showShortcut:any;
- rando:any;
- test_keys:any=[];
- commandNames:any=[];
- excelCommands:any;
-
- currentShortcut:any={};
- q:any =[];
- playerReady:boolean = false;
- multiPressCounter:number = 0;
- success:boolean = false;
- failure:boolean = false;
- correctAnswer:number=0;
-
-
- constructor(private _commandServ:CommandsService, private _microsoftExcel:MicrosoftExcelService,  private sharedData: SharedServiceService ) { }
- 
- ngOnInit() {
-  this.sharedData.currentMessage.subscribe(message =>this.message = message)
-  console.log("from gameboard" + this.message);
-  
-  
-// Gets random x, y values based on the size of the container
-  var y = window.innerWidth;
-  var x = window.innerHeight;
-    var randomX = Math.floor(Math.random()*x);
-    var randomY = Math.floor(Math.random()*y);
-    return [randomX,randomY];
-
-}//end of ngOnInit
-=======
+ message: any;
  value = 0;
  name: string;
  names: any = [];
@@ -81,7 +29,6 @@ export class GameboardComponent {
  test_keys: any = [];
  commandNames: any = [];
  excelCommands: any;
->>>>>>> 33993e667e18a25a4a3cd22f3b8026eaac00ff59
 
  currentShortcut: any = {};
  q: any = []; // questions that have been displayed to the user
@@ -94,7 +41,7 @@ export class GameboardComponent {
 
  constructor(
    private _commandServ: CommandsService,
-   private _changeDet: ChangeDetectorRef
+   private _microsoftExcel: MicrosoftExcelService
    ) { }
 //   ngOnInit() {
 //   // Gets random x, y values based on the size of the container
@@ -154,7 +101,6 @@ export class GameboardComponent {
       ++this.multiPressCounter;
     }
     this.playerReady = true;
-    this._changeDet.detectChanges();
   }
 
     showSuccessMessage() {
@@ -171,11 +117,7 @@ export class GameboardComponent {
     }, 1500);
   }
 
-  justInCase() {
-    setTimeout(() => {
-    this._changeDet.detectChanges();
-    }, 1500);
-  }
+
 
   startNextQuestion() {
     console.log('q array that user has seen');
@@ -187,10 +129,9 @@ export class GameboardComponent {
     console.log('q array after startNextQuestion()');
     console.log(this.q);
     // this._changeDet.detectChanges();
-    this.justInCase();
+    
   }
 
-<<<<<<< HEAD
   getRandomCommand(){
     if(this.message == "adobePhotoshop"){  
       let rando = Math.floor(Math.random() * this._commandServ.commands.length); 
@@ -202,23 +143,10 @@ export class GameboardComponent {
         return this._microsoftExcel.commands[rando];
     }
     }
-=======
-  getRandomCommand() {
-    const rando = Math.floor(Math.random() * this._commandServ.commands.length);
-    console.log('Random number is: ' + rando);
-    return this._commandServ.commands[rando];
-  }
->>>>>>> 33993e667e18a25a4a3cd22f3b8026eaac00ff59
 
   score() {
     this.correctAnswer++;
   }
 
-<<<<<<< HEAD
- 
-
-}//end of class
-=======
 } // end of class
->>>>>>> 33993e667e18a25a4a3cd22f3b8026eaac00ff59
 
